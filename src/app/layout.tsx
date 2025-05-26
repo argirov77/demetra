8440030ea7939d3fbbb9587be192816e91b9bc20
argirov77 <164051115+argirov77@users.noreply.github.com>
2025-05-26 14:49:06 +0300

    fix google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,40 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Demetra Media',
-  description: 'Demetra Media transforms affiliate marketing through innovative strategies, dynamic comparison websites, and a global network—delivering outstanding results for your business.',
+  description:
+    'Demetra Media transforms affiliate marketing through innovative strategies, dynamic comparison websites, and a global network—delivering outstanding results for your business.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-  <head>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KWRWKBVR');</script>
-<!-- End Google Tag Manager -->
-</head>
       <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWRWKBVR"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-{children}</body>
+        
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KWRWKBVR');`,
+          }}
+        />
+
+        
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWRWKBVR"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+
+        {children}
+      </body>
     </html>
   )
 }
